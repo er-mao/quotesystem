162b6de03e7314459238168dcc1a6dacb749b4ba
ermao <563075083@qq.com>
2018-03-13 20:34:19 +0800

    添加 models，包括几个Class，修改 setting 中的时区，语言信息

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Order, Project, Kind
 
-# Register your models here.
+class ProjectInline(admin.StackedInline):
+    model = Project
+    extra = 10
+
+class OrderAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,   {'fields': ['order_name']}),
+        ("日期", {'fields': ['order_time']}),
+    ]
+    inlines = [ProjectInline]
+    
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Kind)
--- order/models.py
+++ order/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Kind(models.Model):
+    kind = models.CharField(max_length=20)
+    def __str__(self):
+        return self.kind
+
+class Order(models.Model):
+    order_name = models.CharField(max_length=20)
+    order_time = models.DateTimeField()
+    def __str__(self):
+        return self.order_name
+
+class Project(models.Model):
+    order_of_project = models.ForeignKey(Order, on_delete=models.CASCADE)
+    project_index = models.CharField(max_length=20)
+    project_kind = models.ForeignKey(Kind, on_delete=models.CASCADE)
+    project_describtion = models.TextField()
+    project_count = models.IntegerField(default=1)
+    project_price = models.IntegerField(default=0)
+    def __str__(self):
+        return self.project_describtion
+
+class Provider(models.Model):
+    provider_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    provider_order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    provider_price = models.CharField(max_length=20)
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    def __str__(self):
+        return self.provider_name
